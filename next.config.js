3e5080b892c77c5be589bee15b8d52286330585d
deegha1 <deegha@myleet.com>
2019-04-20 18:43:15 +0800

    remove minification

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const withSass = require('@zeit/next-sass')
 const withCSS = require('@zeit/next-css')
-
+const TerserPlugin = require('terser-webpack-plugin')
 const webpack = require('webpack')
 
 module.exports = withCSS(withSass({
@@ -8,16 +8,9 @@ module.exports = withCSS(withSass({
   cssLoaderOptions: {
     importLoaders: 1,
     localIdentName: "[local]___[hash:base64:5]",
-  },
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      config.optimization.minimizer = [new TerserPlugin({
-        parallel: true,
-        sourceMap: true
-      })]
-    }
-    return config
   }
 }))
 
 //target: 'serverless',
+
+
